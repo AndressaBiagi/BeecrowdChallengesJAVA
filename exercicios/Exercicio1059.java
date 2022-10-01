5514ae3d2637e1c103c933fa043adcccf60c5b00
AndressaBiagi <andressajbiagi@gmail.com>
2022-10-01 18:17:47 -0300

    correção 1059 e add 1060 e 1064.

--- exercicios/Exercicio1059.java
+++ exercicios/Exercicio1059.java
@@ -1,12 +1,17 @@
 /* 23-09-2022
 */
 package br.com.beecrowd.exercicios;
-
-import java.util.Scanner;
+/*Faça um programa que mostre os números pares entre 1 e 100, inclusive. */
 
 public class Exercicio1059 {
-    //1060, 1064, 1061, 1066, 1067 
+    // 1060, 1064, 1061, 1066, 1067
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+
+        }
+
     }
 }
